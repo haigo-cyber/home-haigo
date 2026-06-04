322df63aa36935e89d3e567fe6d55175e255341f
haigo-cyber <heiko.seiffert@gmail.com>
2026-06-04 17:50:30 +0200

    Tab Projekte neu aufgesetz

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – cacht nur die App-Hülle, niemals Google-API-Antworten.
-const CACHE = "todo-shell-v14";
+const CACHE = "todo-shell-v17";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {

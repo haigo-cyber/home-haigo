d6539991873bb4ac96bd478353e2e79b25240424
haigo-cyber <heiko.seiffert@gmail.com>
2026-08-28 00:32:03 +0200

    Visualiserung und Projekt entfernen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – cacht nur die App-Hülle, niemals Google-API-Antworten.
-const CACHE = "todo-shell-v68";
+const CACHE = "todo-shell-v69";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {

0e372696ced427eae364071662fe96f221c2e2d3
haigo-cyber <heiko.seiffert@gmail.com>
2026-08-29 12:56:58 +0200

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker – cacht nur die App-Hülle, niemals Google-API-Antworten.
-const CACHE = "todo-shell-v86";
+const CACHE = "todo-shell-v87";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
